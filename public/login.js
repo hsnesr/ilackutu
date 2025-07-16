0fd0ea25f4df62cc449744282be70940e2ba1c12
Palladaon <hsnesrofficial@gmail.com>
2025-07-16 12:18:41 +0300

    1

--- public/login.js
+++ public/login.js
@@ -12,7 +12,7 @@ async function login() {
   }
 
   try {
-    const res = await fetch("https://hsnesr.store/api", {
+    const res = await fetch("/api/index.js", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ username: user, password: pass }),
