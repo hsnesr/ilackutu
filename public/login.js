70d121a26de02ffc9509b2883348020c0945a215
Palladaon <hsnesrofficial@gmail.com>
2025-07-16 12:49:39 +0300

    fix: doğru API endpoint

--- public/login.js
+++ public/login.js
@@ -12,7 +12,7 @@ async function login() {
   }
 
   try {
-    const res = await fetch("/api/index.js", {
+    const res = await fetch("/api", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ username: user, password: pass }),
