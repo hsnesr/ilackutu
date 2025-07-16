572d7b50d880879ac7bd497f2a0335ef00f4807a
Palladaon <hsnesrofficial@gmail.com>
2025-07-16 13:17:07 +0300

    add: jsonwebtoken for JWT support

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
-import 'dotenv/config';  // veya require('dotenv').config();
+import 'dotenv/config';
+import jwt from 'jsonwebtoken';
 
 export default function handler(req, res) {
   if (req.method !== "POST") {
@@ -10,6 +11,7 @@ export default function handler(req, res) {
 
   const ADMIN_USER = process.env.ADMIN_USER;
   const ADMIN_PASS = process.env.ADMIN_PASS;
+  const JWT_SECRET = process.env.JWT_SECRET;
 
   if (!username || !password) {
     res.status(400).json({ error: "Kullanıcı adı ve şifre gerekli." });
@@ -17,8 +19,7 @@ export default function handler(req, res) {
   }
 
   if (username === ADMIN_USER && password === ADMIN_PASS) {
-    // Token üretme (JWT önerilir, base64 basit örnek)
-    const token = Buffer.from(`${username}:${password}`).toString("base64");
+    const token = jwt.sign({ username }, JWT_SECRET, { expiresIn: "1h" }); // 1 saatlik geçerlilik
     res.status(200).json({ token });
   } else {
     res.status(401).json({ error: "Geçersiz kullanıcı adı veya şifre." });
